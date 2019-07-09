46168af1c5ee5489092e43a92f8f0e32a78e3dc2
Onida <OnidaSurshana@gmail.com>
2019-07-09 17:12:56 -0400

    added files on javascript

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
-
+"use strict";
 function startGame(){
-  alert();
+
+var myFirstQuestion = "Whats your name?"
+
+var name = prompt(myFirstQuestion)
+var myFirstReply = "Hello " + name + "my name is Ricardo Collymore";
+alert(myFirstReply);
+
+var mySecondQuestion = "Is my middle name Alexander";
+
+var secondResponse = prompt(mySecondQuestion).toLowerCase(); 
+
+if (secondResponse === "yes" || secondResponse === "y"  ) {
+  alert("correct");
+} else {
+  alert("incorrect");
+}
+
 }
 
 startGame();
