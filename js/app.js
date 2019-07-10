d43c727a2f5fb57e55e228e51a465abc1f738dca
Onida <OnidaSurshana@gmail.com>
2019-07-10 09:26:13 -0400

    added files on javascript

--- js/app.js
+++ js/app.js
@@ -17,6 +17,32 @@ if (secondResponse === "yes" || secondResponse === "y"  ) {
   alert("incorrect");
 }
 
+var myThirdQuestion = "I prefer cats to dogs, is this true?"
+
+var thirdResponse = prompt(myThirdQuestion).toLowerCase();//make everything the user types become lower case.
+if (thirdResponse === "no" || secondResponse === "n" ) {
+  alert("correct");
+} else {
+  alert("incorrect");
+}
+
+var myFourthQuestion = "My favorite country is Russia, not Barbados, is this true?"
+
+var fourthResponse = prompt(myFourthQuestion)
+if (fourthResponse === "yes" || thirdResponse === "y" ) {
+  alert("correct");
+} else{
+  alert("incorrect")
+}
+
+var myFithQuestion = "My biggest fear is being drowned in a tsunami, but i never bothered to learn how to swim. is this true?"
+
+var fithResponse = prompt(myFithQuestion)
+if (fithResponse === "yes" || fithResponse === "y" ) {
+  alert("correct");
+} else{
+  alert("incorrect")
+}
 }
 
 startGame();
